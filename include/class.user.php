f065d3c2780318d2e82769669ed536cc5969b1ac
Johnathandoejd <111465895+Johnathandoejd@users.noreply.github.com>
2022-08-18 17:46:31 +0100

    Update class.user.php

--- include/class.user.php
+++ include/class.user.php
@@ -228,13 +228,13 @@ class USER {
         $mail->SMTPDebug = 0;
         $mail->SMTPAuth = true;
         $mail->SMTPSecure = "";
-        $mail->Host = "lorentbk.herokuapp.com";
+        $mail->Host = "lorentwestern.com";
         $mail->Port = 587;
         $mail->AddAddress($email);
-        $mail->Username = "support@lorentbk.herokuapp.com";
+        $mail->Username = "support@lorentwestern.com";
         $mail->Password = "1lorentBank!";
-        $mail->SetFrom('support@lorentbk.herokuapp.com', 'LOrent Western');
-        $mail->AddReplyTo("support@lorentbk.herokuapp.com", "LOrent Western");
+        $mail->SetFrom('support@lorentwestern.com', 'LOrent Western');
+        $mail->AddReplyTo("support@lorentwestern.com", "LOrent Western");
         $mail->Subject = $subject;
         $mail->MsgHTML($messag);
         $mail->Send();
